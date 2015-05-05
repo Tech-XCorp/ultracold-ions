fe75020b2e74705ec0dd11ed15a72dd5a17be371
Dominic Meiser <dmeiser@txcorp.com>
2015-05-05 17:00:38 -0600

    Revert "A couple of fixes in the Boris updater."
    This reverts commit 0907d07f8181bb18ab74f230334f1effcfe73a2c.

--- ucilib/BorisUpdater.py
+++ ucilib/BorisUpdater.py
@@ -1,7 +1,7 @@
 # vi: ts=4 sw=4
 
 import numpy
-import Ptcls
+import ptcls
 import pyopencl as cl
 import pyopencl.array as cl_array
 import sys
@@ -19,36 +19,34 @@ class BorisUpdater():
             self.queue = cl.CommandQueue(self.ctx,
                 properties=cl.command_queue_properties.PROFILING_ENABLE)
 
-    def update(self, xd, yd, zd, vxd, vyd, vzd, qd, md, accelerations,
-            t, dt, numSteps):
+    def update(self, xd, yd, zd, vxd, vyd, vzd, qd, md, accelerations, t, dt):
 
-        for i in range(numSteps):
-    # First half kick
-            axd = cl_array.zeros_like(xd)
-            ayd = cl_array.zeros_like(xd)
-            azd = cl_array.zeros_like(xd)
-            for acc in accelerations:
-                acc.computeAcc(xd, yd, zd, vxd, vyd, vzd, qd, md,
-                        axd, ayd, azd, t)
-            vxd += (0.5 * dt) * axd
-            vyd += (0.5 * dt) * ayd
-            vzd += (0.5 * dt) * azd
-            
+# First half kick
+        axd = cl_array.zeros_like(xd)
+        ayd = cl_array.zeros_like(xd)
+        azd = cl_array.zeros_like(xd)
+        for acc in accelerations:
+            acc.computeAcc(xd, yd, zd, vxd, vyd, vzd, qd, md,
+                    axd, ayd, azd, t)
+        vxd += (0.5 * dt) * axd
+        vyd += (0.5 * dt) * ayd
+        vzd += (0.5 * dt) * azd
+        
 
-    # Advance positions
-            xd += dt * vxd
-            yd += dt * vyd
-            zd += dt * vzd
-            
-    #Second half kick
-            
-            axd.fill(0.0, self.queue)
-            ayd.fill(0.0, self.queue)
-            azd.fill(0.0, self.queue)
-            for acc in accelerations:
-                acc.computeAcc(xd, yd, zd, vxd, vyd, vzd, qd, md,
-                        axd, ayd, azd, t)
-            vxd += (0.5 * dt) * axd
-            vyd += (0.5 * dt) * ayd
-            vzd += (0.5 * dt) * azd
+# Advance positions
+        xd += dt * vxd
+        yd += dt * vyd
+        zd += dt * vzd
+        
+#Second half kick
+        
+        axd.fill(0.0, self.queue)
+        ayd.fill(0.0, self.queue)
+        azd.fill(0.0, self.queue)
+        for acc in accelerations:
+            acc.computeAcc(xd, yd, zd, vxd, vyd, vzd, qd, md,
+                    axd, ayd, azd, t)
+        vxd += (0.5 * dt) * axd
+        vyd += (0.5 * dt) * ayd
+        vzd += (0.5 * dt) * azd
 
